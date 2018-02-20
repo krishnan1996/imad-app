ab34e7e6bf5508d031d019a1176b264e0d74ff40
krishnan1996 <34919876+krishnan1996@users.noreply.github.com>
2018-02-20 18:50:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/articleone.html', function (req, res) {
-  'First para'
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
